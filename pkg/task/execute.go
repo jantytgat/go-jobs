322f4c1826aec51ec2b4038cc52a36a53ec78d0a
Jan Tytgat <jan.tytgat@corelayer.eu>
2024-10-22 11:28:43 +0200

    Removed type Sequence and moved logic into a function
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- pkg/task/execute.go
+++ pkg/task/execute.go
@@ -3,34 +3,26 @@ package task
 import (
 	"context"
 	"log/slog"
-	"sync"
 )
 
-func NewSequence(l *slog.Logger, tasks []Task) Sequence {
-	return Sequence{
-		tasks:     tasks,
-		pipeline:  NewPipeline(l),
-		chResults: make(chan HandlerResult),
+func Execute(ctx context.Context, l *slog.Logger, task Task, r *HandlerRepository) (Result, error) {
+	pipeline := NewPipeline(l)
+	chResults := make(chan HandlerResult)
+	if err := r.Execute(ctx, NewHandlerTaskWithChannel(task, pipeline, chResults)); err != nil {
+		return Result{}, err
 	}
+
+	// Wait for the result of the current task
+	result := <-chResults
+	return Result{
+		Status: result.Status,
+		Error:  result.Error,
+	}, nil
 }
 
-type Sequence struct {
-	tasks   []Task
-	results []Result
-
-	pipeline  *Pipeline
-	chResults chan HandlerResult
-
-	mux sync.Mutex
-}
-
-func (s *Sequence) Execute(ctx context.Context, r *HandlerRepository) error {
-	// Copy the tasks in a local variable to avoid data races
-	tasks := make([]Task, len(s.tasks))
-	s.mux.Lock()
-	copy(tasks, s.tasks)
-	s.mux.Unlock()
-
+func ExecuteSequence(ctx context.Context, l *slog.Logger, tasks []Task, r *HandlerRepository) ([]Result, error) {
+	pipeline := NewPipeline(l)
+	chResults := make(chan HandlerResult)
 	results := make([]Result, len(tasks))
 	for i, task := range tasks {
 		// If the HandlerPool cannot be found in the HandlerRepository, the repository will first try to register
@@ -38,19 +30,16 @@ func (s *Sequence) Execute(ctx context.Context, r *HandlerRepository) error {
 		// We cannot proceed with the execution of the sequence, so we return the error to be handled by the caller.
 		// The handler pool will send the result of the task to s.chResults.
 		// Any data that needs to be passed on through the sequence of tasks is stored in the pipeline by the task handler.
-		if err := r.Execute(ctx, NewHandlerTaskWithChannel(task, s.pipeline, s.chResults)); err != nil {
-			return err
+		if err := r.Execute(ctx, NewHandlerTaskWithChannel(task, pipeline, chResults)); err != nil {
+			return nil, err
 		}
 
 		// Wait for the result of the current task
-		result := <-s.chResults
+		result := <-chResults
 		results[i] = Result{
 			Status: result.Status,
 			Error:  result.Error,
 		}
 	}
-	s.mux.Lock()
-	s.results = results
-	s.mux.Unlock()
-	return nil
+	return results, nil
 }
